e44050d883d74ed485506ddf223de03b8ec1d32b
hoshi <hoshimamoru@team-lab.com>
2015-03-16 21:22:42 +0900

    update

--- testSelenium/src/testSelenium/AI2048.java
+++ testSelenium/src/testSelenium/AI2048.java
@@ -1,7 +1,6 @@
 package testSelenium;
 
 import java.util.Arrays;
-import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -10,6 +9,26 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AI2048 {
+
+	public static class XorShift {
+		private long x, y, z, w;
+
+		{
+			x = 123456789;
+			y = 362436069;
+			z = 521288629;
+			w = 88675123;
+		}
+
+		protected int next() {
+			long t = (x ^ x << 11);
+			x = y;
+			y = z;
+			z = w;
+			w = (w ^ w >>> 19 ^ t ^ t >>> 8);
+			return (int) w & ((1 << 31) - 1);
+		}
+	}
 
 	public static void main(String[] args) {
 		try {
@@ -32,7 +51,7 @@ public class AI2048 {
 		}
 	}
 
-	Random random = new Random();
+	XorShift random = new XorShift();
 	WebDriver driver;
 	WebElement body, tileContainer, keep;
 
@@ -49,7 +68,6 @@ public class AI2048 {
 			Arrow next = think();
 			if (next != null) {
 				body.sendKeys(next.key);
-				Thread.sleep(50);
 			} else {
 				break;
 			}
@@ -71,49 +89,29 @@ public class AI2048 {
 		Arrow res = null;
 		int value = Integer.MIN_VALUE;
 		for (Arrow arrow : Arrow.values()) {
-			int tmp[] = Arrays.copyOf(board, 16);
-
-			//			System.out.println(arrow);
-			//			System.out.println("Before");
-			//			for (int i = 0; i < 4; i++) {
-			//				for (int j = 0; j < 4; j++) {
-			//					System.out.print(tmp[i * 4 + j] + " ");
-			//				}
-			//				System.out.println();
-			//			}
-			if (step(tmp, arrow)) {
-				int tmpValue = dfs(tmp, 5);
+			state tmp = new state(board);
+			if (tmp.step(arrow)) {
+				int tmpValue = dfs(tmp, 3);
 				if (tmpValue > value) {
 					value = tmpValue;
 					res = arrow;
 				}
 			}
-			//			System.out.println("after");
-			//			for (int i = 0; i < 4; i++) {
-			//				for (int j = 0; j < 4; j++) {
-			//					System.out.print(tmp[i * 4 + j] + " ");
-			//				}
-			//				System.out.println();
-			//			}
-			//			System.out.println();
 		}
 		System.out.println(res);
 		return res;
 	}
 
-	public int dfs(int board[], int depth) {
-		if (depth == 0) {
-			return calcValue(board);
-		}
+	public int dfs(state board, int depth) {
 		int value = 0;
 		for (int i = 0; i < 3; i++) {
-			int tmp[] = Arrays.copyOf(board, 16);
-			putTile(tmp);
+			state tmp = new state(board);
+			tmp.putTile();
 			int tmpValue = 0;
 			for (Arrow arrow : Arrow.values()) {
-				int next[] = Arrays.copyOf(tmp, 16);
-				if (step(next, arrow)) {
-					tmpValue = Math.max(tmpValue, dfs(next, depth - 1));
+				state next = new state(tmp);
+				if (next.step(arrow)) {
+					tmpValue = Math.max(tmpValue, (depth == 0 ? next.value : dfs(next, depth - 1)));
 				}
 			}
 			value += tmpValue;
@@ -121,127 +119,133 @@ public class AI2048 {
 		return value;
 	}
 
-	private int vi[] = new int[16];
+	class state {
+		int board[];
+		int value;
 
-	public void putTile(int board[]) {
-		int index = 0;
-		for (int i = 0; i < 16; i++) {
-			if (board[i] == 0) {
-				vi[index] = i;
-				index++;
-			}
+		public state(int board[]) {
+			this.board = Arrays.copyOf(board, 16);
+			value = 0;
 		}
-		index = random.nextInt(index);
-		board[vi[index]] = (random.nextInt(10) == 0 ? 4 : 2);
-	}
 
-	public boolean step(int board[], Arrow arrow) {
-		boolean ok = false;
-		if (arrow == Arrow.up) {
-			for (int i = 0; i < 4; i++) {
-				int index = 0;
-				for (int j = 4; j < 16; j += 4) {
-					if (board[i + j] != 0) {
-						while (index < j) {
-							if (board[index + i] == 0) {
-								board[index + i] += board[i + j];
-								board[i + j] = 0;
-								ok = true;
-								break;
-							} else if (board[index + i] == board[i + j]) {
-								board[index + i] += board[i + j];
-								board[i + j] = 0;
-								ok = true;
+		public state(state s) {
+			board = Arrays.copyOf(s.board, 16);
+			value = s.value;
+		}
+
+		public void putTile() {
+			int vi[] = new int[16];
+			int index = 0;
+			for (int i = 0; i < 16; i++) {
+				if (board[i] == 0) {
+					vi[index] = i;
+					index++;
+				}
+			}
+			index = random.next() % index;
+			board[vi[index]] = (random.next() % 10 == 0 ? 4 : 2);
+		}
+
+		public boolean step(Arrow arrow) {
+			boolean ok = false;
+			if (arrow == Arrow.up) {
+				for (int i = 0; i < 4; i++) {
+					int index = 0;
+					for (int j = 4; j < 16; j += 4) {
+						if (board[i + j] != 0) {
+							while (index < j) {
+								if (board[index + i] == 0) {
+									board[index + i] = board[i + j];
+									board[i + j] = 0;
+									ok = true;
+									break;
+								} else if (board[index + i] == board[i + j]) {
+									board[index + i] <<= 1;
+									board[i + j] = 0;
+									ok = true;
+									value += board[index + i] * board[index + i];
+									index += 4;
+									break;
+								}
 								index += 4;
-								break;
 							}
-							index += 4;
 						}
 					}
 				}
-			}
-		} else if (arrow == Arrow.down) {
-			for (int i = 0; i < 4; i++) {
-				int index = 12;
-				for (int j = 8; j >= 0; j -= 4) {
-					if (board[i + j] != 0) {
-						while (index > j) {
-							if (board[index + i] == 0) {
-								board[index + i] += board[i + j];
-								board[i + j] = 0;
-								ok = true;
-								break;
-							} else if (board[index + i] == board[i + j]) {
-								board[index + i] += board[i + j];
-								board[i + j] = 0;
-								ok = true;
+			} else if (arrow == Arrow.down) {
+				for (int i = 0; i < 4; i++) {
+					int index = 12;
+					for (int j = 8; j >= 0; j -= 4) {
+						if (board[i + j] != 0) {
+							while (index > j) {
+								if (board[index + i] == 0) {
+									board[index + i] = board[i + j];
+									board[i + j] = 0;
+									ok = true;
+									break;
+								} else if (board[index + i] == board[i + j]) {
+									board[index + i] <<= 1;
+									board[i + j] = 0;
+									ok = true;
+									value += board[index + i] * board[index + i];
+									index -= 4;
+									break;
+								}
 								index -= 4;
-								break;
 							}
-							index -= 4;
 						}
 					}
 				}
-			}
-		} else if (arrow == Arrow.right) {
-			for (int i = 0; i < 16; i += 4) {
-				int index = 3;
-				for (int j = 2; j >= 0; j--) {
-					if (board[i + j] != 0) {
-						while (index > j) {
-							if (board[index + i] == 0) {
-								board[index + i] += board[i + j];
-								board[i + j] = 0;
-								ok = true;
-								break;
-							} else if (board[index + i] == board[i + j]) {
-								board[index + i] += board[i + j];
-								board[i + j] = 0;
-								ok = true;
+			} else if (arrow == Arrow.right) {
+				for (int i = 0; i < 16; i += 4) {
+					int index = 3;
+					for (int j = 2; j >= 0; j--) {
+						if (board[i + j] != 0) {
+							while (index > j) {
+								if (board[index + i] == 0) {
+									board[index + i] = board[i + j];
+									board[i + j] = 0;
+									ok = true;
+									break;
+								} else if (board[index + i] == board[i + j]) {
+									board[index + i] <<= 1;
+									board[i + j] = 0;
+									ok = true;
+									value += board[index + i] * board[index + i];
+									index--;
+									break;
+								}
 								index--;
-								break;
 							}
-							index--;
 						}
 					}
 				}
-			}
-		} else if (arrow == Arrow.left) {
-			for (int i = 0; i < 16; i += 4) {
-				int index = 0;
-				for (int j = 1; j < 4; j++) {
-					if (board[i + j] != 0) {
-						while (index < j) {
-							if (board[index + i] == 0) {
-								board[index + i] = board[i + j];
-								board[i + j] = 0;
-								ok = true;
-								break;
-							} else if (board[index + i] == board[i + j]) {
-								board[index + i] += board[i + j];
-								board[i + j] = 0;
-								ok = true;
+			} else if (arrow == Arrow.left) {
+				for (int i = 0; i < 16; i += 4) {
+					int index = 0;
+					for (int j = 1; j < 4; j++) {
+						if (board[i + j] != 0) {
+							while (index < j) {
+								if (board[index + i] == 0) {
+									board[index + i] = board[i + j];
+									board[i + j] = 0;
+									ok = true;
+									break;
+								} else if (board[index + i] == board[i + j]) {
+									board[index + i] <<= 1;
+									board[i + j] = 0;
+									ok = true;
+									value += board[index + i] * board[index + i];
+									index++;
+									break;
+								}
 								index++;
-								break;
 							}
-							index++;
 						}
 					}
 				}
 			}
+			return ok;
 		}
-		return ok;
-	}
-
-	public int calcValue(int board[]) {
-		int value = 0;
-		for (int num : board) {
-			if (num == 0) {
-				value++;
-			} else {
-				value += num * num;
-			}
-		}
-		return value;
 	}
 }
