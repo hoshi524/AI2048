ec217680e97aeb2a71bbc95a6d3209570dba71c3
hoshi <hoshi524@gmail.com>
2015-03-21 04:19:50 +0900

    平均:43000くらい

--- testSelenium/src/testSelenium/AI2048.java
+++ testSelenium/src/testSelenium/AI2048.java
@@ -1,6 +1,8 @@
 package testSelenium;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -22,13 +24,22 @@ public class AI2048 {
 			w = 88675123;
 		}
 
-		protected int next() {
+		int next() {
 			long t = (x ^ x << 11);
 			x = y;
 			y = z;
 			z = w;
 			w = (w ^ w >>> 19 ^ t ^ t >>> 8);
 			return (int) w & ((1 << 31) - 1);
+		}
+
+		long nextLong() {
+			long t = (x ^ x << 11);
+			x = y;
+			y = z;
+			z = w;
+			w = (w ^ w >>> 19 ^ t ^ t >>> 8);
+			return w & ((1 << 31) - 1);
 		}
 	}
 
@@ -49,6 +60,7 @@ public class AI2048 {
 		int sum = 0;
 		for (int x = 0; x < n; x++) {
 			state state = new state();
+			long start = System.nanoTime();
 			while (state.putTile()) {
 				Arrow a = solve(Arrays.copyOf(state.board, 16));
 				if (a == null) {
@@ -57,12 +69,19 @@ public class AI2048 {
 				state.step(a);
 			}
 			state.print();
+
+			System.out.println("time : " + (System.nanoTime() - start) / 1000);
 			System.out.println("score : " + state.score);
 
 			sum += state.score;
 			System.out.println("average : " + (double) sum / (x + 1));
 		}
 	}
+
+	static final int n = 4;
+	static final int nn = n * n;
+	Map<Integer, Integer> toA = new HashMap<>();
+	long hashMap[][] = new long[nn][nn];
 
 	enum Arrow {
 		down(Keys.ARROW_DOWN), //
@@ -80,6 +99,16 @@ public class AI2048 {
 	XorShift random = new XorShift();
 	WebDriver driver;
 	WebElement body, tileContainer, keep;
+
+	{
+		toA.put(0, 0);
+		for (int i = 0; i < nn; i++) {
+			toA.put(1 << (i + 1), i + 1);
+			for (int j = 0; j < nn; j++) {
+				hashMap[i][j] = random.nextLong();
+			}
+		}
+	}
 
 	void solve() throws Exception {
 		driver = new FirefoxDriver();
@@ -102,26 +131,37 @@ public class AI2048 {
 	}
 
 	Arrow think() {
-		int board[] = new int[16];
+		int board[] = new int[nn];
 		{// setTile
 			for (WebElement tile : tileContainer.findElements(By.className("tile"))) {
 				String classes[] = tile.getAttribute("class").split(" ");
 				int value = Integer.parseInt(classes[1].split("-")[1]);
 				int x = Integer.parseInt(classes[2].split("-")[2]) - 1;
 				int y = Integer.parseInt(classes[2].split("-")[3]) - 1;
-				board[y * 4 + x] = value;
+				board[y * n + x] = toA.get(value);
 			}
 		}
 		return solve(board);
 	}
 
+	static final int map_depth = 4;
+	@SuppressWarnings("unchecked")
+	Map<Long, Integer> memo[] = new Map[map_depth + 1];
+	{
+		for (int i = 0; i <= map_depth; i++) {
+			memo[i] = new HashMap<>();
+		}
+	}
+
 	Arrow solve(int board[]) {
 		Arrow res = null;
 		int value = Integer.MIN_VALUE;
+		for (int i = 0; i <= map_depth; i++)
+			memo[i].clear();
 		for (Arrow arrow : Arrow.values()) {
 			state tmp = new state(board);
 			if (tmp.step(arrow)) {
-				int tmpValue = dfs(tmp, 3);
+				int tmpValue = dfs(tmp, map_depth);
 				if (tmpValue > value) {
 					value = tmpValue;
 					res = arrow;
@@ -132,20 +172,56 @@ public class AI2048 {
 	}
 
 	int dfs(state board, int depth) {
+		long key = board.hash();
+		Integer x = memo[depth].get(key);
+		if (x != null)
+			return x;
+		int count = 0;
 		int value = 0;
-		for (int i = 0; i < 4; i++) {
-			state tmp = new state(board);
-			if (tmp.putTile()) {
-				int tmpValue = 0;
-				for (Arrow arrow : Arrow.values()) {
-					state next = new state(tmp);
-					if (next.step(arrow)) {
-						tmpValue = Math.max(tmpValue, (depth == 0 ? next.score : dfs(next, depth - 1)));
+		for (int i = 0; i < nn; i++) {
+			if (board.board[i] == 0) {
+				count++;
+				{
+					state tmp = new state(board);
+					tmp.board[i] = 1;
+					int tmpValue = 0;
+					for (Arrow arrow : Arrow.values()) {
+						state next = new state(tmp);
+						if (next.step(arrow)) {
+							tmpValue = Math.max(tmpValue, (depth == 0 ? next.score : dfs(next, depth - 1)));
+						}
 					}
+					value += tmpValue * 9;
 				}
-				value += tmpValue;
+				//				{
+				//					state tmp = new state(board);
+				//					tmp.board[i] = 2;
+				//					int tmpValue = 0;
+				//					for (Arrow arrow : Arrow.values()) {
+				//						state next = new state(tmp);
+				//						if (next.step(arrow)) {
+				//							tmpValue = Math.max(tmpValue, (depth == 0 ? next.score : dfs(next, depth - 1)));
+				//						}
+				//					}
+				//					value += tmpValue;
+				//				}
 			}
 		}
+		value /= count;
+		//		for (int i = 0; i < 2; i++) {
+		//			state tmp = new state(board);
+		//			if (tmp.putTile()) {
+		//				int tmpValue = 0;
+		//				for (Arrow arrow : Arrow.values()) {
+		//					state next = new state(tmp);
+		//					if (next.step(arrow)) {
+		//						tmpValue = Math.max(tmpValue, (depth == 0 ? next.score : dfs(next, depth - 1)));
+		//					}
+		//				}
+		//				value += tmpValue;
+		//			}
+		//		}
+		memo[depth].put(key, value);
 		return value;
 	}
 
@@ -154,22 +230,22 @@ public class AI2048 {
 		int score = 0;
 
 		public state() {
-			board = new int[16];
+			board = new int[nn];
 		}
 
 		public state(int board[]) {
-			this.board = Arrays.copyOf(board, 16);
+			this.board = Arrays.copyOf(board, nn);
 		}
 
 		public state(state s) {
-			board = Arrays.copyOf(s.board, 16);
+			board = Arrays.copyOf(s.board, nn);
 			score = s.score;
 		}
 
 		public boolean putTile() {
-			int vi[] = new int[16];
+			int vi[] = new int[nn];
 			int index = 0;
-			for (int i = 0; i < 16; i++) {
+			for (int i = 0; i < nn; i++) {
 				if (board[i] == 0) {
 					vi[index] = i;
 					index++;
@@ -178,14 +254,14 @@ public class AI2048 {
 			if (index == 0)
 				return false;
 			index = random.next() % index;
-			board[vi[index]] = (random.next() % 10 == 0 ? 4 : 2);
+			board[vi[index]] = (random.next() % 10 == 0 ? 2 : 1);
 			return true;
 		}
 
 		public void print() {
-			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 4; j++) {
-					System.out.print(String.format("%5d", board[i * 4 + j]));
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					System.out.print(String.format("%3d", board[i * n + j]));
 				}
 				System.out.println();
 			}
@@ -206,10 +282,10 @@ public class AI2048 {
 									ok = true;
 									break;
 								} else if (board[index + i] == board[i + j]) {
-									board[index + i] <<= 1;
+									board[index + i]++;
 									board[i + j] = 0;
 									ok = true;
-									score += board[index + i];
+									score += 1 << board[index + i];
 									index += 4;
 									break;
 								}
@@ -230,10 +306,10 @@ public class AI2048 {
 									ok = true;
 									break;
 								} else if (board[index + i] == board[i + j]) {
-									board[index + i] <<= 1;
+									board[index + i]++;
 									board[i + j] = 0;
 									ok = true;
-									score += board[index + i];
+									score += 1 << board[index + i];
 									index -= 4;
 									break;
 								}
@@ -254,10 +330,10 @@ public class AI2048 {
 									ok = true;
 									break;
 								} else if (board[index + i] == board[i + j]) {
-									board[index + i] <<= 1;
+									board[index + i]++;
 									board[i + j] = 0;
 									ok = true;
-									score += board[index + i];
+									score += 1 << board[index + i];
 									index--;
 									break;
 								}
@@ -278,10 +354,10 @@ public class AI2048 {
 									ok = true;
 									break;
 								} else if (board[index + i] == board[i + j]) {
-									board[index + i] <<= 1;
+									board[index + i]++;
 									board[i + j] = 0;
 									ok = true;
-									score += board[index + i];
+									score += 1 << board[index + i];
 									index++;
 									break;
 								}
@@ -292,6 +368,13 @@ public class AI2048 {
 				}
 			}
 			return ok;
+		}
+
+		long hash() {
+			return hashMap[0][board[0]] ^ hashMap[1][board[1]] ^ hashMap[2][board[2]] ^ hashMap[3][board[3]]
+					^ hashMap[4][board[4]] ^ hashMap[5][board[5]] ^ hashMap[6][board[6]] ^ hashMap[7][board[7]]
+					^ hashMap[8][board[8]] ^ hashMap[9][board[9]] ^ hashMap[10][board[10]] ^ hashMap[11][board[11]]
+					^ hashMap[12][board[12]] ^ hashMap[13][board[13]] ^ hashMap[14][board[14]] ^ hashMap[15][board[15]];
 		}
 	}
 }
